b3157e3f01bf9a3b59d8a2903311d1b5f3cd213c
Javiii454 <jbenitez@ctsformacion.com>
2025-09-15 10:57:24 +0200

    15/09/25Commit2

---
+++ Assets/Scripts/Repaso.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+public class Repaso : MonoBehaviour
+{
+
+   [SerializeField]private int variableInt = 2;
+
+    public float variableFloat = 4.5f;
+
+
+    public string variableString = "Me quedan 2 telediarios ayuda porfavor";
+
+    public bool variableBool = false;
+
+    public int[] arrayInt = new int[5] {12, 4, 8, 9, 0};
+
+    public List<int> listInt = new List<int>(9) {8, 9, 0};
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int numero = 7;
+        if (numero == 7)
+        {
+
+        }
+
+        else if (numero == 3)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        if (numero == 7)
+            //transfrom.position = Vector3.zero;
+
+        //for (int i = 0; i < length; i++)
+        {
+
+        }
+        //foreach (var item in collection)
+        {
+            
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
